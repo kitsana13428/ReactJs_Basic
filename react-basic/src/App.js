d353ba65ee2afce5f05ca65d9f8cc4d25ff76105
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-02 21:29:56 +0700

    JSX Dynamic Data

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,13 +1,13 @@
+import Item from "./component/item";
+
 const Title = () => <h1>รายรับ รายจ่าย</h1>
 const Description = () => <p>บันทึกข้อมูลในแต่ละวัน</p>
-const Item = () => <li>ค่าเดินทาง <span> -500</span></li>
 const Transation = () => {
   return (
     <ul>
         <Item />
         <Item />
-        <Item />
-        <Item />
+    
           {/* <li>ค่าอาหาร <span> -1500</span></li>
           <li>เงินเดือน <span> +10000</span></li> */}
       </ul> 
@@ -18,11 +18,9 @@ const Transation = () => {
 function App() {
   return (
     <div> 
-      <center>
         <Title />
         <Description />
-        <Transation />
-      </center>
+        <Transation />  
     </div>
   );
 }
---
+++ react-basic/src/component/item.js
@@ -0,0 +1,9 @@
+const Item = () => {
+    const name = "ค่าเดินทาง"
+    const amount = 500
+    return (
+    <li> {name} <span> -{amount} </span></li>
+    );
+}
+
+export default Item
