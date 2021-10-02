a104b9d7038e890870725ae6d9d0a6369ddcb077
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-02 21:08:23 +0700

    Nested Component

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+const Title = () => <h1>รายรับ รายจ่าย</h1>
+const Description = () => <p>บันทึกข้อมูลในแต่ละวัน</p>
+const Item = () => <li>ค่าเดินทาง <span> -500</span></li>
+const Transation = () => {
+  return (
+    <ul>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+          {/* <li>ค่าอาหาร <span> -1500</span></li>
+          <li>เงินเดือน <span> +10000</span></li> */}
+      </ul> 
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <center>
+        <Title />
+        <Description />
+        <Transation />
+      </center>
     </div>
   );
 }
