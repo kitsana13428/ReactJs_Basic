e64b99980d2e1956ead3a1ece246088f8850ae7d
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-06 22:36:53 +0700

    key Props uuid

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,22 +1,23 @@
 import Item from "./component/item";
 import "./App.css"
+import { v4 as uuidv4 } from 'uuid'; //อิมพอตยูไอดีมาใช้ สร้างไอดีอัตโนมัติ
 
 const Title = () => <h1>แอพบัญชี รายรับ-รายจ่าย</h1>
 // const Description = () => <p>บันทึกข้อมูลในแต่ละวัน</p>
 const Transation = () => {
   const data = [
-    {id:1, title:"ค่ารถ", amount:"3000"},
-    {id:2, title:"ค่าอาหาร", amount:"2000"},
-    {id:3, title:"ค่าน้ำมัน", amount:"1500"},
-    {id:4, title:"ค่ายา", amount:"1000"},
-    {id:5, title:"ค่าขนม", amount:"300"},
-    {id:6, title:"ค่าครีม", amount:"900"}
+    {title:"ค่ารถ", amount:"3000"},
+    {title:"ค่าอาหาร", amount:"2000"},
+    {title:"ค่าน้ำมัน", amount:"1500"},
+    {title:"ค่ายา", amount:"1000"},
+    {title:"ค่าขนม", amount:"300"},
+    {title:"ค่าครีม", amount:"900"}
   ]
   return (
     <ul className="item-list">
        {data.map((element) => {
         //  return <Item title = {element.title} amount = {element.amount}/> //array.map ลูปอาเรย์มาแสดง
-         return <Item {...element} key = {element.id} /> //ดึงมาใช้แบบสั้น
+         return <Item {...element} key = {uuidv4()} /> //ดึงมาใช้แบบสั้น และใช้ uid
        })
        }
   
