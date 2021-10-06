5f28f47c12129724c1c3650bf34525292dad33ab
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-06 22:28:50 +0700

    key props

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -5,18 +5,18 @@ const Title = () => <h1>แอพบัญชี รายรับ-รายจ
 // const Description = () => <p>บันทึกข้อมูลในแต่ละวัน</p>
 const Transation = () => {
   const data = [
-    {title:"ค่าอาหาร", amount:"2000"},
-    {title:"ค่ารถ", amount:"3000"},
-    {title:"ค่าน้ำมัน", amount:"1500"},
-    {title:"ค่าขนม", amount:"300"},
-    {title:"ค่ายา", amount:"1000"},
-    {title:"ค่าครีม", amount:"900"}
+    {id:1, title:"ค่ารถ", amount:"3000"},
+    {id:2, title:"ค่าอาหาร", amount:"2000"},
+    {id:3, title:"ค่าน้ำมัน", amount:"1500"},
+    {id:4, title:"ค่ายา", amount:"1000"},
+    {id:5, title:"ค่าขนม", amount:"300"},
+    {id:6, title:"ค่าครีม", amount:"900"}
   ]
   return (
     <ul className="item-list">
        {data.map((element) => {
         //  return <Item title = {element.title} amount = {element.amount}/> //array.map ลูปอาเรย์มาแสดง
-         return <Item {...element} /> //ดึงมาใช้แบบสั้น
+         return <Item {...element} key = {element.id} /> //ดึงมาใช้แบบสั้น
        })
        }
   
