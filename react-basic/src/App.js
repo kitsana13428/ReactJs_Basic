b17ae7cee18852cda53ddda2b371ba2658d5fd51
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-06 22:23:14 +0700

    spread operator

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -7,12 +7,16 @@ const Transation = () => {
   const data = [
     {title:"ค่าอาหาร", amount:"2000"},
     {title:"ค่ารถ", amount:"3000"},
-    {title:"ค่าน้ำมัน", amount:"1500"}
+    {title:"ค่าน้ำมัน", amount:"1500"},
+    {title:"ค่าขนม", amount:"300"},
+    {title:"ค่ายา", amount:"1000"},
+    {title:"ค่าครีม", amount:"900"}
   ]
   return (
     <ul className="item-list">
        {data.map((element) => {
-         return <Item title = {element.title} amount = {element.amount}/> //array.map ลูปอาเรย์มาแสดง
+        //  return <Item title = {element.title} amount = {element.amount}/> //array.map ลูปอาเรย์มาแสดง
+         return <Item {...element} /> //ดึงมาใช้แบบสั้น
        })
        }
   
