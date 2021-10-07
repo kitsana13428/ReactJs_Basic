63e221ac254c125aa6e6c45d8a317887e099b8fd
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-07 22:11:30 +0700

    Form & Style

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,6 +1,7 @@
 import Item from "./component/item";
 import "./App.css"
 import { v4 as uuidv4 } from 'uuid'; //อิมพอตยูไอดีมาใช้ สร้างไอดีอัตโนมัติ
+import FormComponent from "./component/FormComponent"; //ดึงเข้ามาใช้งาน
 
 const Title = () => <h1>แอพบัญชี รายรับ-รายจ่าย</h1>
 // const Description = () => <p>บันทึกข้อมูลในแต่ละวัน</p>
@@ -9,9 +10,8 @@ const Transation = () => {
     {title:"ค่ารถ", amount:3000},
     {title:"ค่าอาหาร", amount:2000},
     {title:"ค่าน้ำมัน", amount:1500},
-    {title:"ค่ายา", amount:1000},
-    {title:"ค่าขนม", amount:300},
-    {title:"ค่าครีม", amount:900}
+    {title:"ค่ายา", amount:1000}
+    
   ]
   return (
     <ul className="item-list">
@@ -19,18 +19,20 @@ const Transation = () => {
         //  return <Item title = {element.title} amount = {element.amount}/> //array.map ลูปอาเรย์มาแสดง
          return <Item {...element} key = {uuidv4()} /> //ดึงมาใช้แบบสั้น และใช้ uid
        })
-       }
+     }
   
       </ul> 
   );
 }
 
 
-function App() { 
+function App() { //เรียกใช้คอมโพแนน
   return (
     <div className="container"> 
-        <Title />
-        <Transation />    
+        <Title /> 
+        <FormComponent />
+        <Transation />  
+          
     </div>
   );
 }
---
+++ react-basic/src/component/FormComponent.js
@@ -0,0 +1,23 @@
+import './FormComponent.css'
+
+const FormComponent = () => {
+    return(
+        <div>
+            <form>
+                <div className="form-control">
+                    <label>ชื่อรายการ</label>
+                    <input type="text" placeholder= "ระบุชื่อรายการ"/>
+                </div>
+                <div className="form-control">
+                    <label>จำนวนเงิน</label>
+                    <input type="number" placeholder= "ระบุจำนวนเงิน"/>
+                </div>
+                <div>
+                    <button type="submit" className="btn">+เพิ่มข้อมูล</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default FormComponent
