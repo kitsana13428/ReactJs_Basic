7d89fea54e54b9b9b7a4c9e6a18be7ff4feb4d20
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-17 21:22:04 +0700

    Context API
    Data Context

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -3,7 +3,7 @@ import Transaction from "./component/Transaction";
 import FormComponent from "./component/FormComponent"; //ดึงเข้ามาใช้งาน
 import "./App.css"
 import { useState } from "react";
-
+import DataContext from "./data/DataContext";
 
 
 const Title = () => <h1>แอพบัญชี รายรับ-รายจ่าย</h1>
@@ -21,12 +21,13 @@ function App() { //เรียกใช้คอมโพแนน
       })
     }
   return (
-    <div className="container"> 
+  <DataContext.Provider value={"kitsana"}>
+     <div className="container"> 
         <Title /> 
         <FormComponent onAddItem = {onAddNewItem}/>
         <Transaction items = {items}/>  
-          
-    </div>
+    </div>  
+  </DataContext.Provider>
   );
 }
 
--- react-basic/src/component/Transaction.js
+++ react-basic/src/component/Transaction.js
@@ -1,16 +1,22 @@
 import Item from "./item";
+import DataContext from "../data/DataContext";
 
 const Transaction = (props) => {
     const {items} = props
     return (
-      <ul className="item-list">
+      <div>
+        <ul className="item-list">
          {items.map((element) => {
           //  return <Item title = {element.title} amount = {element.amount}/> //array.map ลูปอาเรย์มาแสดง
            return <Item {...element} key = {element.id} /> //ดึงมาใช้แบบสั้น และใช้ uid
          })
        }
-    
         </ul> 
+
+        <DataContext.Consumer>
+          {value=> <p>{value}</p>}
+        </DataContext.Consumer> 
+      </div>
     );
   }
 
