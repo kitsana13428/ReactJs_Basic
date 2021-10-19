75d66ab59784a19220ed18e7684c9386e346ce01
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-10-19 09:40:05 +0700

    ีuseEffect
     setReport Income & expense

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,26 +2,42 @@
 import Transaction from "./component/Transaction";
 import FormComponent from "./component/FormComponent"; //ดึงเข้ามาใช้งาน
 import "./App.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DataContext from "./data/DataContext";
 import ReportComponent from "./component/ReportComponent";
 
 function App() { //เรียกใช้คอมโพแนน
 
-  const initData = [
-      
-  ]
-    const [items,setItems] = useState(initData)
+    // const initState = [
+    //   {id:1,title: "ค่าเช่า", amount: -2000},
+    //   {id:1,title: "ค่าอาหาร", amount: -1500},
+    //   {id:1,title: "ค่าเดินทาง", amount: -3000},
+    //   {id:1,title: "เงินเดือน", amount: 25000}
+    // ]
+    const [items,setItems] = useState([]) //[] = อาเรย์เปล่าๆ
+
+    const [reportIncome, setReportIncome] = useState(0) //สร้างตัวแปร รีพอตรายรับ และ จ่าย
+    const [reportExpense, setReportExpense] = useState(0)
+
     const onAddNewItem = (newItem)=> {
       setItems((prevItem) => {
         return [newItem,...prevItem] //ใหม่สุดอยู่ข้างบน
       })
     }
+
+    useEffect(() => { //เรียกใช้งาน useEffect
+      const amounts = items.map(items => items.amount) //ลูปข้อมูลอาเรย์แล้วเก็บในตัวแปร amount
+      const income = amounts.filter(element => element > 0).reduce((total,element) => total+=element,0)//กรองค่าที่มากกว่า 0 เก็บในตัวแปร และ reduce คือการรวมค่าอาเรย์  
+      const expense = (amounts.filter(element => element < 0).reduce((total,element) => total+=element,0))*-1 //*-1 เพื่อ ค่าลบ ให้เป้น บวก
+      setReportIncome(income)
+      setReportExpense(expense)
+    },[items,reportIncome,reportExpense])
+
   return (
   <DataContext.Provider value={
     {
-      income : 5000,
-      expense : -8000
+      income : reportIncome,
+      expense : reportExpense
     }
   }>
      <div className="container"> 
